e53ed5ab91a295a0ff53dffa6c298fc529f35567
Yamileth Rodriguez <46757368+yrodriguez83@users.noreply.github.com>
2022-06-24 12:08:25 -0500

    Mac Build
    -- Deleted C++ modules and converted to them to Blueprint based classes since Mac and Windows can only be cross platform collaborated strictly on Blueprint based systems :,(

--- Source/mobile_mkrdemo/mobile_mkrdemo.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "mobile_mkrdemo.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, mobile_mkrdemo, "mobile_mkrdemo" );
- 
